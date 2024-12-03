a2814a06673e4924d2140eafe5207637c8ae2afb
Larvact <tobyg7794@hotmail.com>
2024-12-03 22:17:48 +0000

    Added log and mongo template code to check the database for uniqueness prior to inserting a new student document

--- src/main/java/toby/springboot/learn/configuration/DatabaseInitialiserConfiguration.java
+++ src/main/java/toby/springboot/learn/configuration/DatabaseInitialiserConfiguration.java
@@ -1,10 +1,15 @@
 package toby.springboot.learn.configuration;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import toby.springboot.learn.documents.Student;
 import toby.springboot.learn.entities.Address;
 import toby.springboot.learn.entities.Gender;
@@ -18,23 +23,42 @@ import java.util.List;
 @Configuration
 public class DatabaseInitialiserConfiguration
 {
+    private static final Logger log = LoggerFactory.getLogger(DatabaseInitialiserConfiguration.class);
+
     @ConditionalOnMissingBean(CommandLineRunner.class)
     @Bean
-    CommandLineRunner provideCommandLineRunner(final StudentRepository repository)
+    CommandLineRunner provideCommandLineRunner(final StudentRepository repository, final MongoTemplate mongoTemplate)
     {
+        final var email = "tobyg7794@hotmail.com";
         return args ->
         {
             final var student = new Student(
                     "Toby",
                     "Goddard",
-                    "tobyg7794@hotmail.com",
+                    email,
                     Gender.MALE,
                     new Address("United Kingdom", "London", "W14 9NA"),
                     Instant.now(),
                     List.of(Subject.MATH),
                     new BigDecimal("132.65")
             );
-            repository.insert(student);
+            final var query = new Query()
+                    .addCriteria(Criteria.where("email")
+                            .is("tobyg7794@hotmail.com"));
+
+            final var students = mongoTemplate.find(query, Student.class);
+            if(students.size() > 1)
+            {
+                throw new IllegalStateException();
+            }
+            else if (students.size() == 1)
+            {
+                log.warn("Student with email [{}] already exists", email);
+            }
+            else
+            {
+                repository.insert(student);
+            }
         };
     }
 

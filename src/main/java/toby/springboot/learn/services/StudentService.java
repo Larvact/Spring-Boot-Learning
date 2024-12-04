a02c8a0a4c26babfb2e23a9d068808701d12292d
Larvact <tobyg7794@hotmail.com>
2024-12-04 17:13:58 +0000

    Added service implmenetation and unit test to retrieve all students

--- src/main/java/toby/springboot/learn/services/StudentService.java
+++ src/main/java/toby/springboot/learn/services/StudentService.java
@@ -1,18 +1,26 @@
 package toby.springboot.learn.services;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import toby.springboot.learn.documents.Student;
+import toby.springboot.learn.repositories.StudentRepository;
 
-import java.util.Collections;
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class StudentService
 {
+    private final StudentRepository studentRepository;
+
     public List<Student> getAllStudents()
     {
-        return Collections.emptyList();
+        return studentRepository.findAll();
     }
+
+
+
+
 
     public Student createStudent(final Student student)
     {
---
+++ src/test/java/toby/springboot/learn/services/TestStudentService.java
@@ -0,0 +1,65 @@
+package toby.springboot.learn.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import toby.springboot.learn.documents.Student;
+import toby.springboot.learn.entities.Address;
+import toby.springboot.learn.entities.Gender;
+import toby.springboot.learn.entities.Subject;
+import toby.springboot.learn.repositories.StudentRepository;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+class TestStudentService
+{
+    private static final String STUDENT_ID = UUID.randomUUID().toString();
+    private static final Student STUDENT = new Student(
+            "Toby",
+            "Goddard",
+            "tobyg7794@hotmail.com",
+            Gender.MALE,
+            new Address("United Kingdom", "London", "W14 9NA"),
+            Instant.now(),
+            List.of(Subject.MATH),
+            new BigDecimal("132.65"));
+
+    @Autowired
+    private StudentService studentService;
+    @MockitoBean
+    private StudentRepository studentRepository;
+
+    @TestConfiguration
+    static class StudentServiceConfiguration
+    {
+        @Bean
+        StudentService studentService(final StudentRepository studentRepository)
+        {
+            return new StudentService(studentRepository);
+        }
+    }
+
+    @Test
+    void givenStudents_getAllStudents_returnsTheList()
+    {
+        Mockito.when(studentRepository.findAll()).thenReturn(List.of(STUDENT));
+
+        assertThat(studentService.getAllStudents())
+                .hasSize(1)
+                .containsExactly(STUDENT);
+    }
+
+
+}
